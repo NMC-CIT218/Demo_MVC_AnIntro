05741bf0b90a1196f788c3e73bd331e10d70be31
John Velis <john@johnvelis.com>
2016-01-09 10:35:25 -0500

    IC

---
+++ Demo_MVC_AnIntro/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_MVC_AnIntro.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        }
+    }
+}
---
+++ Demo_MVC_AnIntro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_MVC_AnIntro.MvcApplication" Language="C#" %>
